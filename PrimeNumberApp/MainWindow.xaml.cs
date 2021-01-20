a64a5d8fa283c95e7a86d83a1a4119ddae976f98
Vanický Adam <vanicky.adam.2017@skola.ssps.cz>
2021-01-20 15:49:53 +0100

    Přidejte soubory projektu.

---
+++ PrimeNumberApp/MainWindow.xaml.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PrimeNumberApp
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void UlohaJednaStart_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int max = 10;
+                PrimeNumberCalculator pnc = new PrimeNumberCalculator();
+                List<int> prime = new List<int>();
+                int[] pri = prime.ToArray();
+                switch (((ComboBoxItem)cbUlohaJedna.SelectedItem).Content.ToString())
+                {
+                    case "Menší než deset":
+                        max = 10;
+                        pnc = new PrimeNumberCalculator();
+                        prime = pnc.Prime_Numbers_Lower_Than_Ten(max);
+                        pri = prime.ToArray();
+                        FirstOutput.Text = $"Menší než deset: ";
+                        
+                        break;
+                    case "Obsahuje právě 2 jedničky":
+                        max = 150;
+                        pnc = new PrimeNumberCalculator();
+                        prime = pnc.Prime_Numbers_With_Two_Ones_Together_Up_To_OneHundredFifty(max);
+                        pri = prime.ToArray();
+                        FirstOutput.Text = $"Obsahuje právě 2 jedničky: ";
+                        break;
+                    case "Dvojciferná prvočísla do třiceti":
+                        max = 30;
+                        pnc = new PrimeNumberCalculator();
+                        prime = pnc.Two_Digit_Prime_Numbers_Up_To_Thirty(max);
+                        pri = prime.ToArray();
+                        FirstOutput.Text = $"Dvojciferná prvočísla do třiceti: ";
+                        break;
+                }
+                for (int i = 0; i < pri.Length; i++)
+                {
+                    FirstOutput.Text += $"{pri[i]} ";
+                    System.Threading.Thread.Sleep(1000);
+                }
+                FirstOutput.Text += "Úloha ukončena";
+            }
+            catch
+            {
+                throw new Exception("Vyberte kritérium");
+            }
+        }
+
+        private void UlohaDvaStart_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int max;
+                PrimeNumberCalculator pnc = new PrimeNumberCalculator();
+                List<int> prime = new List<int>();
+                int[] pri = prime.ToArray();
+                switch (((ComboBoxItem)cbUlohaDva.SelectedItem).Content.ToString())
+                {
+
+                    case "Menší než deset":
+                        max = 10;
+                        pnc = new PrimeNumberCalculator();
+                        prime = pnc.Prime_Numbers_Lower_Than_Ten(max);
+                        pri = prime.ToArray();
+                        SecondOutput.Text = $"Menší než deset: ";
+                        break;
+                    case "Obsahuje právě 2 jedničky":
+                        max = 150;
+                        pnc = new PrimeNumberCalculator();
+                        prime = pnc.Prime_Numbers_With_Two_Ones_Together_Up_To_OneHundredFifty(max);
+                        pri = prime.ToArray();
+                        SecondOutput.Text = $"Obsahuje právě 2 jedničky: ";
+                        break;
+                    case "Dvojciferná prvočísla do třiceti":
+                        max = 30;
+                        pnc = new PrimeNumberCalculator();
+                        prime = pnc.Two_Digit_Prime_Numbers_Up_To_Thirty(max);
+                        pri = prime.ToArray();
+                        SecondOutput.Text = $"Dvojciferná prvočísla do třiceti: ";
+                        break;
+                }
+                for (int i = 0; i < pri.Length; i++)
+                {
+                    SecondOutput.Text += $"{pri[i]} ";
+                    System.Threading.Thread.Sleep(1000);
+                }
+                SecondOutput.Text += "Úloha ukončena";
+            }
+            catch
+            {
+                throw new Exception("Vyberte kritérium");
+            }
+        }
+
+        private void UlohaTriStart_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int max = 10;
+                PrimeNumberCalculator pnc = new PrimeNumberCalculator();
+                List<int> prime = new List<int>();
+                int[] pri = prime.ToArray();
+                switch (((ComboBoxItem)cbUlohaTri.SelectedItem).Content.ToString())
+                {
+                    case "Menší než deset":
+                        max = 10;
+                        pnc = new PrimeNumberCalculator();
+                        prime = pnc.Prime_Numbers_Lower_Than_Ten(max);
+                        pri = prime.ToArray();
+                        ThirdOutput.Text = $"Menší než deset: ";
+                        break;
+                    case "Obsahuje právě 2 jedničky":
+                        max = 150;
+                        pnc = new PrimeNumberCalculator();
+                        prime = pnc.Prime_Numbers_With_Two_Ones_Together_Up_To_OneHundredFifty(max);
+                        pri = prime.ToArray();
+                        ThirdOutput.Text = $"Obsahuje právě 2 jedničky: ";
+                        break;
+                    case "Dvojciferná prvočísla do třiceti":
+                        max = 30;
+                        pnc = new PrimeNumberCalculator();
+                        prime = pnc.Two_Digit_Prime_Numbers_Up_To_Thirty(max);
+                        pri = prime.ToArray();
+                        ThirdOutput.Text = $"Dvojciferná prvočísla do třiceti: ";
+                        break;
+                }
+                for (int i = 0; i < pri.Length; i++)
+                {
+                    ThirdOutput.Text += $"{pri[i]} ";
+                    System.Threading.Thread.Sleep(1000);
+                }
+                ThirdOutput.Text += "Úloha ukončena";
+            }
+            catch
+            {
+                throw new Exception("Vyberte kritérium");
+            }
+        }
+    }
+}
---
+++ PrimeNumberApp/PrimeNumberCalculator.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberApp
+{
+    public class PrimeNumberCalculator
+    {
+        public List<int> Prime_Numbers_Lower_Than_Ten(int max)
+        {
+            
+            bool[] is_prime = MakeSieve(max);
+            List<int> prime_nums = new List<int>();
+            for (int i = 2; i < is_prime.Length; i++)
+            {
+                if(is_prime[i] is true)
+                {
+                    prime_nums.Add(i);
+                }
+            }
+            return prime_nums;
+        }
+        public List<int> Prime_Numbers_With_Two_Ones_Together_Up_To_OneHundredFifty(int max)
+        {
+
+            bool[] is_prime = MakeSieve(max);
+            List<int> prime_nums = new List<int>();
+            for (int i = 2; i < is_prime.Length; i++)
+            {
+                string s = i.ToString();
+                if (is_prime[i] && s.Contains("11"))
+                {
+                    prime_nums.Add(i);
+                }
+            }
+            return prime_nums;
+        }
+        public List<int> Two_Digit_Prime_Numbers_Up_To_Thirty(int max)
+        {
+            bool[] is_prime = MakeSieve(max);
+            List<int> prime_nums = new List<int>();
+            for (int i = 2; i < is_prime.Length; i++)
+            {
+                if (is_prime[i] && i >= 10)
+                {
+                    prime_nums.Add(i);
+                }
+            }
+            return prime_nums;
+        }
+
+        private bool[] MakeSieve(int max)
+        {
+
+            bool[] is_prime = new bool[max + 1];
+            for (int i = 2; i <= max; i++) is_prime[i] = true;
+
+
+            for (int i = 2; i <= max; i++)
+            {
+
+                if (is_prime[i])
+                {
+
+                    for (int j = i * 2; j <= max; j += i)
+                        is_prime[j] = false;
+                }
+            }
+            return is_prime;
+        }
+    }
+}
